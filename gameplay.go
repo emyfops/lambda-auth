3fc2f20154060503d295bdfb22400cfc51bf682d
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2025-03-17 19:14:41 -0400

    Fixed protocol version string conversion

--- gameplay.go
+++ gameplay.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"github.com/Tnze/go-mc/chat"
-	"github.com/Tnze/go-mc/data/packetid"
 	"github.com/Tnze/go-mc/net"
-	pk "github.com/Tnze/go-mc/net/packet"
 	"github.com/Tnze/go-mc/yggdrasil/user"
 	"github.com/google/uuid"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"strconv"
 )
 
 var successLogins = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -20,8 +18,5 @@ type NoGamePlay struct{}
 
 // AcceptPlayer Disconnect the player and stop the player handling
 func (n *NoGamePlay) AcceptPlayer(_ string, _ uuid.UUID, _ *user.PublicKey, _ []user.Property, protocol int32, conn *net.Conn) {
-	conn.WritePacket(pk.Marshal(
-		packetid.ClientboundDisconnect, chat.Text("login complete")))
-
-	successLogins.With(prometheus.Labels{"protocol": string(protocol)}).Inc()
+	successLogins.With(prometheus.Labels{"protocol": strconv.Itoa(int(protocol))}).Inc()
 }
